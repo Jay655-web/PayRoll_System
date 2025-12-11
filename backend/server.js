e05088c70135150808983d1d6312d143bce2990a
Adjei Edmond <adjeiedmond582@gmai.com>
2025-12-11 16:41:36 +0000

    Update

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,9 @@ app.use(cors(
             'http://127.0.0.1:5500',
             'https://pay-roll-system-3vvbpz9yt-jay655-webs-projects.vercel.app',
             'https://jay655-web.github.io/PayRoll_System/',
-            'https://pay-roll-system2-ujki-dr0dvwgg8-jay655-webs-projects.vercel.app'
+            // 'https://pay-roll-system2-ujki-dr0dvwgg8-jay655-webs-projects.vercel.app',
+            'https://pay-roll-system2-ujki-dr0dvwgg8-jay655-webs-projects.vercel.app/Index.html',
+            ''
         ],
         method: "GET,POST,PUT,DELETE",
         credentials: true
